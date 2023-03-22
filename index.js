32100ad4fcdb71b356d1fddfddb98fb2edef9072
ngzhenghui94 <ngzhenghui94@hotmail.com>
2023-03-22 12:05:28 +0000

    to repo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new TelegramBot(process.env.TELEGRAMBOTAPIKEY, { polling: true });
 const api = new ChatGPTAPI({
     apiKey: process.env.APIKEY,
     debug: true,
-    temperature: 0.9,
+    temperature: 1.4,
     promptPrefix: "",
 })
 
@@ -21,41 +21,53 @@ let objArray = {}
 bot.on('message', async (msg) => {
     console.log(msg)
     if (idArray.includes(msg.chat.id)) {
-        let textMsg = msg.text
+        const textMsg = msg.text
         // send a follow-up
         // console.log(objArray)
         // console.log(idArray)
-        let res = await api.sendMessage(msg.text, {
-            conversationId: objArray[msg.chat.id][0],
-            parentMessageId: objArray[msg.chat.id][1],
-            lastSent: moment().format('YYYY-MM-DD HH:mm:ss')
-        })
-        if (res.token.total_tokens >= 1000) {
-            console.log("reseting convo")
-            objArray[msg.chat.id] = [0, 0]
-        } else {
-            console.log("convo within token")
+        try {
+
+            let res = await api.sendMessage(msg.text, {
+                conversationId: objArray[msg.chat.id][0],
+                parentMessageId: objArray[msg.chat.id][1],
+                lastSent: moment().format('YYYY-MM-DD HH:mm:ss')
+            })
+            if (res.token.total_tokens >= 1000) {
+                console.log("reseting convo")
+                objArray[msg.chat.id] = [0, 0]
+            } else {
+                console.log("convo within token")
+                objArray[msg.chat.id] = [res.conversationId, res.id]
+            }
+
+            // console.log("Follow up convo")
+            // console.log(res)
+
+            bot.sendMessage(msg.chat.id, res.text).then((msg) => {
+                bot.sendMessage(1708060707, "<code>" + textMsg + JSON.stringify(msg) + "</code>", { parse_mode: "HTML" })
+                // console.log(textMsg)
+            })
+        } catch (e) {
+            bot.sendMessage(msg.chat.id, "Sorry there was an error. Please try again." + e)
+            bot.sendMessage(1708060707, JSON.stringify(e))
+        }
+    } else {
+        try {
+            idArray.push(msg.chat.id)
+            let res = await api.sendMessage(msg.text)
             objArray[msg.chat.id] = [res.conversationId, res.id]
+
+            // console.log("First Convo" + res)
+
+            bot.sendMessage(msg.chat.id, res.text).then((msg) => {
+                bot.sendMessage(1708060707, "<code>" + textMsg + JSON.stringify(msg) + "</code>", { parse_mode: "HTML" })
+                // console.log(msg)
+            })
+        } catch (e) {
+            bot.sendMessage(msg.chat.id, "Sorry there was an error. Please try again." + e)
+            bot.sendMessage(1708060707, JSON.stringify(e))
         }
 
-        // console.log("Follow up convo")
-        // console.log(res)
-
-        bot.sendMessage(msg.chat.id, res.text).then((msg) => {
-            bot.sendMessage(1708060707, "<code>" + JSON.stringify(msg) + "</code>", { parse_mode: "HTML" })
-            console.log(textMsg)
-        })
-    } else {
-        idArray.push(msg.chat.id)
-        let res = await api.sendMessage(msg.text)
-        objArray[msg.chat.id] = [res.conversationId, res.id]
-
-        // console.log("First Convo" + res)
-
-        bot.sendMessage(msg.chat.id, res.text).then((msg) => {
-            bot.sendMessage(1708060707, "<code>" + JSON.stringify(msg) + "</code>", { parse_mode: "HTML" })
-            console.log(textMsg)
-        })
     }
 
 });
